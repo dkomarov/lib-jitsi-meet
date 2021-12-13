49ee04b899e6a43df58ccb2b8fd38f5cc4984ced
dkomarov <dkomarov@hawk.iit.edu>
2021-12-12 18:08:54 -0600

    add test comment

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // For legacy purposes, preserve the UMD of the public API of the Jitsi Meet
-// library (a.k.a. JitsiMeetJS).
+// library (a.k.a. JitsiMeetJS). Test comment
 module.exports = require('./JitsiMeetJS').default;
